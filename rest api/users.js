9e9e40438432a2006d3bd92ccceba545dd0ecb13
SanaNavaei <sana.sarinavaei@gmail.com>
2023-07-10 03:01:31 +0330

    add lists of users and handle all methods

--- rest api/users.js
+++ rest api/users.js
@@ -1,30 +1,65 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(express.json());
+const users = [
+    { id: 1, name: "user1" },
+    { id: 2, name: "user2" },
+];
+
 router.get("/", (req, res, next) => {
     res.status(200).json({
-        message: "Get",
+        message: "Get usrs",
+        users: users,
     });
 });
 
 router.post("/", (req, res, next) => {
+    const { name } = req.body;
+    const id = users.length + 1;
+    const new_user = { id, name };
+    users.push(new_user);
+
     res.status(200).json({
-        message: "Post",
+        message: "New User Created",
+        user: users,
     });
 });
 
 router.put("/:id", (req, res, next) => {
     const userId = req.params.id;
-    res.status(200).json({
-        message: `Update user with ID ${userId}`,
-    });
+    const { name } = req.body;
+    const user = users.findIndex((user) => user.id === parseInt(userId));
+
+    if (user != -1) {
+        users[user].name = name;
+        res.status(200).json({
+            message: `User with ID ${userId} updated with ${name}`,
+            user: users,
+        });
+    } else {
+        res.status(404).json({
+            message: `User with ID ${userId} not found`,
+        });
+    }
 });
 
 router.delete("/:id", (req, res, next) => {
     const userId = req.params.id;
-    res.status(200).json({
-        message: `Delete user with ID ${userId}`,
-    });
+    const user = users.findIndex((user) => user.id === parseInt(userId));
+
+    if (user != -1) {
+        users.splice(user, 1);
+
+        res.status(200).json({
+            message: `User with ID ${userId} deleted`,
+            user: users,
+        });
+    } else {
+        res.status(404).json({
+            message: `User with ID ${userId} not found`,
+        });
+    }
 });
 
 module.exports = router;
